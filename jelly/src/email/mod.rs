e2404073d3e32bc7931f5668123139b4dbe5c173
matclab <mathieu@clabaut.net>
2021-08-18 18:37:23 +0200

    Send mail via multiple provider until one succeed

--- jelly/src/email/mod.rs
+++ jelly/src/email/mod.rs
@@ -1,12 +1,43 @@
-pub mod common; 
+pub use common::Configurable;
+pub use common::Email;
+pub use tera::Context;
+
+use anyhow::anyhow;
+
+pub mod common;
 #[cfg(feature = "email-postmark")]
 pub mod postmark;
-#[cfg(feature = "email-postmark")]
-pub use postmark::Email;
+#[cfg(feature = "email-sendgrid")]
+pub mod sendgrid;
 #[cfg(feature = "email-smtp")]
 pub mod smtp;
-#[cfg(feature = "email-smtp")]
-pub use smtp::Email;
 
-pub use tera::Context as Context;
-pub use common::Configurable ;
+impl Configurable for Email {
+    fn check_conf() {
+        #[cfg(feature = "email-postmark")]
+        postmark::check_conf();
+        #[cfg(feature = "email-smtp")]
+        smtp::check_conf();
+        #[cfg(feature = "email-sendgrid")]
+        sendgrid::check_conf();
+    }
+}
+
+impl Email {
+    pub fn send(self) -> Result<(), anyhow::Error> {
+        let mut res = Result::Err(anyhow!("No email provider configured"));
+        #[cfg(feature = "email-postmark")]
+        if res.is_err() { 
+            res = Email::send_via_postmark(&self);
+        }
+        #[cfg(feature = "email-sendgrid")]
+        if res.is_err() { 
+            res = Email::send_via_sendgrid(&self);
+        }
+        #[cfg(feature = "email-smtp")]
+        if res.is_err() { 
+            res = Email::send_via_smtp(&self);
+        }
+        res
+    }
+}
--- jelly/src/email/smtp.rs
+++ jelly/src/email/smtp.rs
@@ -2,51 +2,44 @@ use std::env::var;
 
 use anyhow::Result;
 
-use super::common::Configurable;
-pub use super::common::Email;
+use super::common::{Email,env_exists_and_not_empty};
+use lettre::message::MultiPart;
 use lettre::transport::smtp::{authentication::Credentials, client::Tls};
 use lettre::{Message, SmtpTransport, Transport};
-use lettre::message::MultiPart;
 
-
-fn env_exists_and_not_empty(env: &str) {
-    if var(env).expect(&format!("{} not set!", env)).is_empty() {
-        panic!("{} is empty", env)
-    }
+/// Check that all needed environment variables are set and not empty.
+pub fn check_conf() {
+    vec![
+        "EMAIL_DEFAULT_FROM",
+        "EMAIL_SMTP_HOST",
+        "EMAIL_SMTP_PORT",
+        "EMAIL_SMTP_USERNAME",
+        "EMAIL_SMTP_PASSWORD",
+    ]
+    .into_iter()
+    .for_each(|env| env_exists_and_not_empty(env));
 }
 
-impl Configurable for Email {
-    /// Check that all needed environment variables are set and not empty.
-    fn check_conf() {
-        vec![
-            "EMAIL_DEFAULT_FROM",
-            "EMAIL_SMTP_HOST",
-            "EMAIL_SMTP_PORT",
-            "EMAIL_SMTP_USERNAME",
-            "EMAIL_SMTP_PASSWORD",
-        ]
-        .into_iter()
-        .for_each(|env| env_exists_and_not_empty(env));
-    }
-}
 impl Email {
     /// Send the email. Relies on you ensuring that `EMAIL_DEFAULT_FROM`,
     /// `EMAIL_SMTP_HOST`, `EMAIL_SMTP_USERNAME`, and `EMAIL_SMTP_PASSWORD`
     /// are set in your `.env`.
-    pub fn send(self) -> Result<(), anyhow::Error> {
+    pub fn send_via_smtp(&self) -> Result<(), anyhow::Error> {
         let host = var("EMAIL_SMTP_HOST").expect("EMAIL_SMTP_HOST not set!");
         let port = var("EMAIL_SMTP_PORT").expect("EMAIL_SMTP_PORT not set!");
         let username = var("EMAIL_SMTP_USERNAME").expect("EMAIL_SMTP_USERNAME not set!");
         let password = var("EMAIL_SMTP_PASSWORD").expect("EMAIL_SMTP_PASSWORD not set!");
-        let reply_to = var("JELLY_SUPPORT_EMAIL").or_else::<anyhow::Error,_>(|_v| Ok(self.from.clone())).unwrap();
+        let reply_to = var("JELLY_SUPPORT_EMAIL")
+            .or_else::<anyhow::Error, _>(|_v| Ok(self.from.clone()))
+            .unwrap();
 
         // TODO REPLY TO with JELLY_SUPPORT_EMAIL env var
         let email = Message::builder()
             .from(self.from.parse()?)
             .reply_to(reply_to.parse()?)
             .to(self.to.parse()?)
-            .subject(self.subject)
-            .multipart(MultiPart::alternative_plain_html(self.body, self.bodyhtml))?;
+            .subject(&self.subject)
+            .multipart(MultiPart::alternative_plain_html(self.body.clone(), self.bodyhtml.clone()))?;
 
         let creds = Credentials::new(username, password);
 
