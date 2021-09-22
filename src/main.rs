3b28960de2bd86dab42f95a3b5ddc04cb5aff0a0
matclab <mathieu@clabaut.net>
2021-09-22 14:49:33 +0200

    Remove wip for tests erroneously commited

--- src/main.rs
+++ src/main.rs
@@ -26,24 +26,3 @@ async fn main() -> io::Result<()> {
         .await
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use jelly::actix_web::{test, web, App};
-
-    #[actix_rt::test]
-    async fn test_index_get() {
-        let mut app = test::init_service(App::new().route("/", web::get().to(index))).await;
-        let req = test::TestRequest::with_header("content-type", "text/plain").to_request();
-        let resp = test::call_service(&mut app, req).await;
-        assert!(resp.status().is_success());
-    }
-
-    #[actix_rt::test]
-    async fn test_index_post() {
-        let mut app = test::init_service(App::new().route("/", web::get().to(index))).await;
-        let req = test::TestRequest::post().uri("/").to_request();
-        let resp = test::call_service(&mut app, req).await;
-        assert!(resp.status().is_client_error());
-    }
-}
